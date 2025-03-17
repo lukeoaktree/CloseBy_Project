693afe5581ce32a15a6828cbb3b89199f3f58b6a
lukeoaktree <lukeoakley26393@gmail.com>
2025-03-17 12:57:48 -0400

    USer email and password stored in MySql Succesfully

--- CloseBy_Backend/db.js
+++ CloseBy_Backend/db.js
@@ -3,11 +3,13 @@ const mysql2 = require("mysql2");
 const connection = mysql2.createConnection({
     host: "localhost",
     user: "root",
-    password: "yourpassword",
-    database: "yourdb"
+    password: "Marlins161",
+    database: "closeby"
 });
 
 connection.connect(err => {
     if (err) throw err;
-    console.log("Connected to the database!");
+    console.log("Connected to the database! Yippee!!");
 });
+
+module.exports = connection;  
