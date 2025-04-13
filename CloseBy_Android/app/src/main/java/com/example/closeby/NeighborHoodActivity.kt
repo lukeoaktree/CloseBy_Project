4d0038baa2afdd65c86c15ea6e76855f74d5c43d
lukeoaktree <lukeoakley26393@gmail.com>
2025-04-13 14:35:00 -0400

    Logout added

--- CloseBy_Android/app/src/main/java/com/example/closeby/NeighborHoodActivity.kt
+++ CloseBy_Android/app/src/main/java/com/example/closeby/NeighborHoodActivity.kt
@@ -2,10 +2,15 @@ package com.example.closeby.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.closeby.CreateNeighborhoodActivity
+import com.example.closeby.LoginActivity
 import com.example.closeby.R
+import com.google.firebase.auth.FirebaseAuth
 
 class NeighborhoodActivity : AppCompatActivity() {
 
@@ -28,4 +33,22 @@ class NeighborhoodActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                Toast.makeText(this, "Logged out successfully", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
